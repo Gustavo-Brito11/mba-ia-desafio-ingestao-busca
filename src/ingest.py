2f49e07e01f4611c5f0df67d08309778d03f9b9d
Gustavo-Brito11 <gustavobsbrito@hotmail.com>
2026-05-26 22:52:05 -0300

    Implement PDF ingestion and embedding storage functionality

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,45 @@
 import os
 from dotenv import load_dotenv
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
+GOOGLE_EMBEDDING_MODEL = os.getenv("GOOGLE_EMBEDDING_MODEL")
+PG_VECTOR_COLLECTION_NAME = os.getenv("PG_VECTOR_COLLECTION_NAME")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def ingest_pdf():
-    pass
+    loader = PyPDFLoader(PDF_PATH)
+    documents = loader.load()
+    
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    chunks = splitter.split_documents(documents)
+    
+    enriched_documents = [
+        Document(
+            page_content=chunk.page_content,
+            metadata={k: v for k, v in chunk.metadata.items() if v not in ["", None]}
+        )
+        for chunk in chunks
+    ]
 
+    ids_documents = [f'doc-{i}' for i in range(len(enriched_documents))]
 
+    embeddings = GoogleGenerativeAIEmbeddings(model=GOOGLE_EMBEDDING_MODEL)
+    
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=PG_VECTOR_COLLECTION_NAME,
+        connection=DATABASE_URL,
+        use_jsonb=True
+    )
+    
+    store.add_documents(documents=enriched_documents, ids=ids_documents)
+    
 if __name__ == "__main__":
     ingest_pdf()
